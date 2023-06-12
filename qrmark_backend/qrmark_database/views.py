d52012e8c731edab0989c61d545836b53af98a9e
ofori_Prince <Pofori034@st.ug.edu.gh>
2023-06-12 18:11:46 +0000

    update

--- qrmark_backend/qrmark_database/views.py
+++ qrmark_backend/qrmark_database/views.py
@@ -30,3 +30,30 @@ class StudentLoginAPI(generics.GenericAPIView):
                     "token" : None                 
                 }
                 return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
+class LecturerLoginAPI(generics.GenericAPIView):
+    serializer_class = LecturerLoginSerializer
+    permission_classes = [permissions.AllowAny]
+    
+    def post(self, request, *args, **kwargs):
+        message = ""
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+            user = serializer.validated_data["user"]
+            _, token = AuthToken.objects.create(user)
+            response_data = {
+                "message" : "Login successful",
+                "data" : UserSerializer(user, context=self.get_serializer_context()).data,
+                "token" : token
+            }
+            return Response(response_data, status=status.HTTP_200_OK)
+        except serializers.ValidationError as e:
+            for field in list(e.detail):
+                message = e.detail[field][0]
+                response_data = {
+                    "message" : message,
+                    "data" : None,
+                    "token" : None                 
+                }
+                return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
